6fd5cf1ef910d3ce3aad06443384998a5237ff1f
Henrique Franco <henrique.frco@gmail.com>
2021-05-18 00:24:10 -0300

    Added interface (TaxService)

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,12 +6,11 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-
 import model.entities.CarRental;
-import model.entities.Invoice;
 import model.entities.Vehicle;
 import model.services.BrazilTaxService;
 import model.services.RentalService;
+import model.services.TaxService;
 
 public class Program {
 
